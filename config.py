21c65761101f2ea2ecb6f10bcf0bc256d24a0b42
Amina Mardiyyah Rufai <39123465+Aminah92@users.noreply.github.com>
2021-08-26 20:05:05 +0000

    Create config.py
    Generic parameters for model training

---
+++ config.py
@@ -0,0 +1,16 @@
+#Config
+dmodel = 512 
+h = 8  #number of heads
+N = 6  #number of layers in decoder and encoder
+en = "..../english.txt"
+fr = ".../french.txt"
+
+src_vocab = len(en)
+trg_vocab = len(fr)
+
+
+
+lr = 0.0001 #random selection
+b1 = 0.9 #beta 1
+b2 = 0.98 #beta 2
+eps = 1e-9
