010011e63775a956476a85f55ececca94b6b29ed
Amina Mardiyyah Rufai <39123465+Aminah92@users.noreply.github.com>
2021-08-27 11:05:29 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 dmodel = 512 
 h = 8  #number of heads
 N = 6  #number of layers in decoder and encoder
+
 en = "..../english.txt"
 fr = ".../french.txt"
 
@@ -9,7 +10,8 @@ src_vocab = len(en)
 trg_vocab = len(fr)
 
 
-
+batch_size = 4
+epoch = 10
 lr = 0.0001 #random selection
 b1 = 0.9 #beta 1
 b2 = 0.98 #beta 2
